f24689fe7162ee8c36b5c024f59617507a9cc7ad
plsainrat <paul.sainrat.external@gdi-simulation.fr>
2025-10-29 10:54:38 +0100

    Upd script
    Gestion des package pas trouve

--- setConf.sh
+++ setConf.sh
@@ -3,26 +3,72 @@ set -e
 
 # === CONFIG ===
 DOTFILES_REPO="git@github.com:plsainrat/dotfiles.git"
-PKGS="git vim i3 alacritty curl"
+PKGS=("git" "vim" "i3" "alacritty" "curl")
 
-# === CHECKS ===
-echo "[*] Updating system..."
+# === DETECTION PACKAGE MNG ===
 if command -v apt &>/dev/null; then
-    sudo apt update && sudo apt install -y $PKGS
+	PKG_MNGR="apt" 
 elif command -v dnf &>/dev/null; then
-    sudo dnf install -y $PKGS
+	PKG_MNGR="dnf" 
 elif command -v pacman &>/dev/null; then
-    sudo pacman -Syu --noconfirm $PKGS
-else
-    echo "Unsupported package manager. Install packages manually: $PKGS"
+	PKG_MNGR="pacman" 
+else 
+	echo " Pas de package manager " 
+	exit 1
 fi
+
+
+# === Updates ===
+echo "[*] Updating system... using : $PKG_MNGR"
+case "$PKG_MNGR" in 
+	apt)
+		sudo apt update -y
+		;;
+	dnf)
+		sudo dnf check-update || true
+		;;
+	pacman)
+		sudo pacman -Sy --noconfirm
+		;;
+esac
+
+# === Install Packages ===
+echo "Installing packages" 
+for pkg in "${PKGS[@]}"; do
+	echo "Installing $pkg ..."
+	case "$PKG_MNGR" in
+	apt)
+		if sudo apt install -y "$pkg"; then
+			echo "$pkg installed."
+		else
+			echo "failed to install $pkg, skipping"
+		fi
+		;;
+	dnf)
+		if sudo dnf install -y "$pkg"; then
+			echo "$pkg installed."
+		else
+			echo "failed to install $pkg, skipping"
+		fi
+		;;
+	pacman)
+		if pacman -S --needed --noconfirm "$pkg"; then
+			echo "$pkg installed."
+		else
+			echo "failed to install $pkg, skipping"
+		fi
+		;;
+	esac
+done
 
 # === DOTFILES ===
 echo "[*] Setting up dotfiles..."
 if [ ! -d "$HOME/.dotfiles" ]; then
     git clone "$DOTFILES_REPO" "$HOME/.dotfiles"
+    git -C "$HOME/.dotfiles" checkout master
 else
     echo "Dotfiles repo already exists, pulling latest..."
+    git -C "$HOME/.dotfiles" checkout master
     git -C "$HOME/.dotfiles" pull
 fi
 
@@ -32,8 +78,7 @@ ln -sf ~/.dotfiles/i3 ~/.config/i3
 ln -sf ~/.dotfiles/vim/.vimrc ~/.vimrc
 
 # === Vim ===
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'PlugInstall --sync' +qa
 
 
